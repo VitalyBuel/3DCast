78c6663756d8479e8a769db59f7620d5bd905395
Буель Витали. Юрьевич <Buel_V@server.llmz>
2021-09-09 15:55:26 +0200

    add fpages

--- django_start/mysite/project/settings.py
+++ django_start/mysite/project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'fpages',
 ]
 
 SITE_ID = 1
