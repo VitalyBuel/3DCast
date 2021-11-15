b3f4f2edd042a76058ffd6d160300765e758e889
VitalyBuel <vitalybuel@gmail.com>
2021-11-15 23:07:54 +0300

    'update'

--- news_portal/prj/newapp/models.py
+++ news_portal/prj/newapp/models.py
@@ -19,10 +19,15 @@ class Author(models.Model):
         self.ratingAuthor = pRat * 3 + cRat
         self.save()
 
+    def __str__(self):
+        return self.authorUser.username
+
 
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
 
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -50,6 +55,8 @@ class Post(models.Model):
 
     def preview(self):
         return self.text[0:123] + '...'
+
+
 
 
 class PostCategory(models.Model):
