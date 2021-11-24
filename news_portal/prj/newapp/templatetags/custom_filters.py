f9760394073cf688ad80a78b4aad51eb5c8edc6f
Буель Витали. Юрьевич <Buel_V@server.llmz>
2021-11-24 16:35:45 +0200

    'update_3.4'

--- news_portal/prj/newapp/templatetags/custom_filters.py
+++ news_portal/prj/newapp/templatetags/custom_filters.py
@@ -7,8 +7,18 @@ register = template.Library()
 @register.filter(name='censor')
 def censor(value):
     censor_words = ['охуеть', 'блядь', 'наитию']
+    fragments = []
+    # Проходимся по всем словам.
     for word in censor_words:
-        if word[3] == value:
-            value = '*****'
-        else:
-            return value
+        # Разбиваем слово на части, и проходимся по ним.
+        for part in range(len(value)):
+            fragments.append(part)
+    for word in censor_words:
+    # Проходимся по всем фрагментам.
+        for fragment in fragments:
+        # Сравниваем фрагмент и искомое слово
+            if word == fragment:
+            # Если они равны, выводим надпись о их нахождении.
+                return "****"
+            else:
+                return word
