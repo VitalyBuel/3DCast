18e023d1d09d6e54b5df8af07d7895543a09edbe
VitalyBuel <62281946+VitalyBuel@users.noreply.github.com>
2022-02-16 13:12:24 +0200

    Delete django_start directory

--- django_start/mysite/simpleapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductList, ProductDetail  # импортируем наше представление
-
-urlpatterns = [
-    path('', ProductList.as_view()),
-    path('<int:pk>', ProductDetail.as_view()),
-]
